5e74f0c083b4ac6512b10481e0e64440a1bfa852
n129586 <ruben.garciapariente@gruposantander.com>
2023-10-17 20:19:32 +0200

    first commit

---
+++ src/main/java/com/example/kakfkaaot/KakfkaAotApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakfkaaot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfkaAotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakfkaAotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kakfkaaot/KakfkaAotApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.kakfkaaot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@EmbeddedKafka
+@SpringBootTest
+class KakfkaAotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
